43d9e961771484bcfa9872c51db6ad71d6407ade
Prabal Sharma <prabal.sharma@suse.com>
2017-06-15 13:53:57 -0700

    script to check if the k8 hoast is ready for cf deployment

---
+++ cf-deploy-teardown/k8s-ready-state-check.sh
@@ -0,0 +1,62 @@
+# Script to determine is the K8s host is "ready" for cf deployment
+
+set -ex
+
+# cgroup memory & swap accounting in /proc/cmdline
+
+cat /proc/cmdline | grep -w "cgroup_enable=memory"
+echo "Verified: cgroug_enable memory"
+
+cat /proc/cmdline | grep -w "swapaccount=1"
+echo "Verified: swapaccount enabled"
+
+# docker info should show overlay2
+
+docker info | grep -w "Storage Driver: overlay2"
+echo "Verified: 'docker info should show overlay2'"
+
+# kube-dns shows 4/4 ready
+
+kube_dns=$(k get pods : | grep "kube-dns-")
+if [[ $kube_dns= == *"4/4 Running"* ]]; then
+  echo "Verified: 'kube-dns shows 4/4 ready'"
+fi
+
+# ntp is installed and running
+
+systemctl list-unit-files | grep -w "ntpd.service"
+echo "Verified: ntp is installed"
+
+systemctl list-unit-files | grep -w "ntpd.service                            enabled"
+echo "Verified: ntp is running"
+
+# "persistent" storage class exists in K8s
+
+k get storageclasses | grep -w "persistent   StorageClass.v1.storage.k8s.io"
+echo "Verified: 'persistent' storage class exists in K8s"
+
+# privileged pods are enabled in K8s
+
+kube_apiserver=$(systemctl status kube-apiserver -l | grep "/usr/bin/hyperkube apiserver" )
+if [[ $kube_apiserver == *"--allow-privileged"* ]]; then
+  echo "Verified: privileged enabled in 'kube-apiserver'"
+fi
+
+kube_apiserver=$(systemctl status kubelet -l | grep "/usr/bin/hyperkube kubelet" )
+if [[ $kube_apiserver == *"--allow-privileged"* ]]; then
+  echo "Verified: privileged enabled in 'kubelet'"
+fi
+
+# dns check for the current hostname resolution
+
+domain_address=$(nslookup cf-dev.io | grep answer: -A 2 | grep Address:)
+domain_address_array=( $domain_address )
+IP=${domain_address_array[1]}
+sudo ifconfig | grep -w "inet addr:$IP"
+echo "Verified: dns check"
+
+# override tasks infinity in systemd configuration
+
+systemctl cat containerd | grep -w "TasksMax=infinity"
+echo "Verified: TasksMax set to infinity"
+
