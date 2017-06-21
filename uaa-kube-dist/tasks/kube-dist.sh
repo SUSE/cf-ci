02a1e63555cba4830a54c08850631b05272f62c5
Andreas Kupries <andreas.kupries@suse.com>
2017-06-21 15:55:13 -0700

    Be verbose.

--- uaa-kube-dist/tasks/kube-dist.sh
+++ uaa-kube-dist/tasks/kube-dist.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:/opt/rubies/ruby-2.3.1/bin
 gem install bosh_cli --no-ri --no-rdoc
 
 # Need xxd for the cert stuff
-apt-get update && apt-get install -qv vim-common
+apt-get update && apt-get install vim-common
 
 tar xf s3.fissile-binary/fissile-* -C /usr/local/bin fissile
 tar xf s3.stampy-binary/stampy-* -C /usr/local/bin stampy
