df17f44ca90c1e8e4659827efea08ac37e84fd82
Prabal Sharma <prabal.sharma@suse.com>
2017-06-23 16:13:34 -0700

    make syntex more readable

--- cf-deploy-teardown/tasks/k8s-ready-state-check.sh
+++ cf-deploy-teardown/tasks/k8s-ready-state-check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Script to determine is the K8s host is "ready" for cf deployment
+FAILED=0
 
 function green() {
   awk '{ print "\033[32mVerified: " $0 "\033[0m" }';
@@ -10,60 +11,63 @@ function red() {
   awk '{ print "\033[31mConfiguration problem detected: " $0 "\033[0m" }';
 }
 
+function status() {
+  if [ $? -eq 0 ]; then
+    echo "$1" | green
+  else
+    echo "$1" | red
+    FAILED=1
+  fi
+}
 # cgroup memory & swap accounting in /proc/cmdline
+grep -wq "cgroup_enable=memory" /proc/cmdline
+status "cgroup_enable memory"
 
-echo "cgroup_enable memory" | ( grep -wq "cgroup_enable=memory" /proc/cmdline && green || red )
-
-
-
-echo "swapaccount enable" | ( grep -wq "swapaccount=1" /proc/cmdline && green || red )
+grep -wq "swapaccount=1" /proc/cmdline
+status "swapaccount enable"
 
 # docker info should show overlay2
 
-echo "docker info should show overlay2" | ( docker info | grep -wq "Storage Driver: overlay2" && green || red )
+docker info 2> /dev/null | grep -wq "Storage Driver: overlay2"
+status "docker info should show overlay2"
 
 # kube-dns shows 4/4 ready
 
-echo "kube-dns should shows 4/4 ready" | (
-    kube_dns=$(kubectl get pods --all-namespaces | grep -q "kube-dns-")
-    [[ $kube_dns == *"4/4 Running"* ]] && green || red
-)
+kube_dns=$(kubectl get pods --all-namespaces | grep -q "kube-dns-")
+[[ $kube_dns == *"4/4 Running"* ]]
+status "kube-dns should shows 4/4 ready"
 
 # ntp is installed and running
 
-
-echo "ntp must be installed and active" | ( systemctl is-active ntpd >& /dev/null && green || red )
+systemctl is-active ntpd >& /dev/null
+status "ntp must be installed and active"
 
 # "persistent" storage class exists in K8s
 
-echo "'persistent' storage class should exist in K8s" | (
-    kubectl get storageclasses |& grep -wq "persistent   StorageClass.v1.storage.k8s.io" && green || red
-)
-
+kubectl get storageclasses |& grep -wq "persistent   StorageClass.v1.storage.k8s.io"
+status "'persistent' storage class should exist in K8s"
 
 # privileged pods are enabled in K8s
 
-echo "Privileged must be enabled in 'kube-apiserver'" | (
-    kube_apiserver=$(systemctl status kube-apiserver -l | grep "/usr/bin/hyperkube apiserver" )
-    [[ $kube_apiserver == *"--allow-privileged"* ]] && green || red
-)
+kube_apiserver=$(systemctl status kube-apiserver -l | grep "/usr/bin/hyperkube apiserver" )
+[[ $kube_apiserver == *"--allow-privileged"* ]]
+status "Privileged must be enabled in 'kube-apiserver'"
 
-echo "Privileged must be enabled in 'kubelet'" | (
-    kubelet=$(systemctl status kubelet -l | grep "/usr/bin/hyperkube kubelet" )
-    [[ $kubelet == *"--allow-privileged"* ]] && green || red
-)
+kubelet=$(systemctl status kubelet -l | grep "/usr/bin/hyperkube kubelet" )
+[[ $kubelet == *"--allow-privileged"* ]]
+status "Privileged must be enabled in 'kubelet'"
 
 # dns check for the current hostname resolution
 
-echo "dns check" | (
-    IP=$(nslookup cf-dev.io | grep answer: -A 2 | grep Address: | sed 's/Address: *//g')
-    #TODO: replace cf-dev.io with $hostname.ci.van when this script is implemented in CI
-    /sbin/ifconfig | grep -wq "inet addr:$IP" && green || red
-)
+IP=$(nslookup cf-dev.io | grep answer: -A 2 | grep Address: | sed 's/Address: *//g')
+#TODO: replace cf-dev.io with $hostname.ci.van when this script is implemented in CI
+/sbin/ifconfig | grep -wq "inet addr:$IP"
+status "dns check"
 
 
 # override tasks infinity in systemd configuration
 
-echo "TasksMax must be set to infinity" | (
-    systemctl cat containerd | grep -wq "TasksMax=infinity" && green || red
-)
+systemctl cat containerd | grep -wq "TasksMax=infinity"
+status "TasksMax must be set to infinity"
+
+exit $FAILED
