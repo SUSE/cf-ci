c568396d3e4aac1759064d77129982bec50ddc77
Prabal Sharma <prabal.sharma@suse.com>
2017-06-27 15:59:48 -0700

    scf yml and uaa yaml in one folder, k8s deploy

--- cf-deploy-teardown/tasks/cf-deploy.sh
+++ cf-deploy-teardown/tasks/cf-deploy.sh
@@ -2,14 +2,20 @@
 
 set -ex
 
-unzip s3.uaa-kube-yml/uaa-kube-* -d uaa-kube-yml
-unzip s3.hcf-kube-yml/hcf-kube-* -d hcf-kube-yml
+env
+# Run the following commands in an environment with kubectl to target this kube cluster:
+kubectl config set-cluster --server=$K8S_HOST_IP $K8S_HOSTNAME
+kubectl config set-context $K8S_HOSTNAME --cluster=$K8S_HOSTNAME
+kubectl config use-context $K8S_HOSTNAME
+
+unzip s3.scf-kube-yml/scf-kube-* -d scf-kube-yml
 
 #Deploy UAA
 kubectl create namespace uaa
-kubectl create -n uaa -f s3.uaa-kube-yml/bosh/
+kubectl create -n uaa -f s3.scf-kube-yml/uaa/bosh/
+kubectl create -n uaa -f s3.scf-kube-yml/uaa/kube-test/exposed-ports.yml
 
 #Deploy CF
 kubectl create namespace cf
-kubectl create -n cf -f s3.hcf-kube-yml/bosh
-kubectl create -n cf -f s3.hcf-kube-yml/bosh-task/post-deployment-setup.yml
+kubectl create -n cf -f s3.scf-kube-yml/bosh
+kubectl create -n cf -f s3.scf-kube-yml/bosh-task/post-deployment-setup.yml
