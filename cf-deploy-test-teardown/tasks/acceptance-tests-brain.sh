02e2ac3b2464246e516b5066b59114cec7fdd36a
Prabal Sharma <prabal.sharma@suse.com>
2017-07-12 16:33:03 -0700

    dockerfile install ruby, smoke tests and teardown added to pipeline, pool lock changed from branch master to k8s-hosts

---
+++ cf-deploy-test-teardown/tasks/acceptance-tests-brain.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+#export k8s-host details from pool
+set -a; source pool.k8s-hosts/metadata; set +a
+
+ssh-keygen -N "" -f /root/.ssh/id_rsa
+sshpass -e ssh-copy-id -o StrictHostKeyChecking=no ${K8S_USER}@${K8S_HOST_IP}
+kubectl config set-cluster --server=$K8S_HOST_IP:$K8S_HOST_PORT $K8S_HOSTNAME
+kubectl config set-context $K8S_HOSTNAME --cluster=$K8S_HOSTNAME
+kubectl config use-context $K8S_HOSTNAME
+
+unzip s3.scf-kube-yml/scf-kube-* -d scf-kube-yml
+
+image=$(cat scf-kube-yml/cf/bosh-task/acceptance-tests-brain.yml | grep "image" | awk -F' ' '{print $2}')
+jsonify() { ruby -r yaml -r json -e 'YAML.load_stream(File.read "'"$1"'").each { |yaml| puts yaml.to_json}'; }
+kubectl run -n cf --attach --restart=Never --image ${image} --overrides="$(jsonify scf-kube-yml/cf/bosh-task/acceptance-tests-brain.yml)" acceptance-tests-brain
--- cf-deploy-test-teardown/tasks/cf-deploy.sh
+++ cf-deploy-test-teardown/tasks/cf-deploy.sh
@@ -8,7 +8,7 @@ set -a; source pool.k8s-hosts/metadata; set +a
 #check k8s host readiness to deploy CF
 ssh-keygen -N "" -f /root/.ssh/id_rsa
 sshpass -e ssh-copy-id -o StrictHostKeyChecking=no ${K8S_USER}@${K8S_HOST_IP}
-ssh -o StrictHostKeyChecking=no ${K8S_USER}@${K8S_HOST_IP} 'bash -s' < cf-ci/cf-deploy-teardown/tasks/k8s-ready-state-check.sh
+ssh -o StrictHostKeyChecking=no ${K8S_USER}@${K8S_HOST_IP} 'bash -s' < cf-ci/cf-deploy-test-teardown/tasks/k8s-ready-state-check.sh
 
 # target the kube cluster
 kubectl config set-cluster --server=${K8S_HOST_IP}:${K8S_HOST_PORT} ${K8S_HOSTNAME}
