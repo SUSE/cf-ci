d470f4f2490235995ba8a6a124e71eac73823b80
Prabal Sharma <prabal.sharma@suse.com>
2017-07-14 10:49:35 -0700

    refactor .sh scripts

--- cf-deploy-test-teardown/tasks/cf-teardown.sh
+++ cf-deploy-test-teardown/tasks/cf-teardown.sh
@@ -7,4 +7,4 @@ set -a; source pool.k8s-hosts/metadata; set +a
 
 ssh-keygen -N "" -f /root/.ssh/id_rsa
 sshpass -e ssh-copy-id -o StrictHostKeyChecking=no ${K8S_USER}@${K8S_HOST_IP}
-ssh -o StrictHostKeyChecking=no $K8S_USER@$K8S_HOST_IP 'kubectl delete namespace uaa; kubectl delete namespace cf'
+ssh -o StrictHostKeyChecking=no ${K8S_USER}@${K8S_HOST_IP} 'kubectl delete namespace uaa; kubectl delete namespace cf'
--- cf-deploy-test-teardown/tasks/smoke-tests.sh
+++ cf-deploy-test-teardown/tasks/smoke-tests.sh
@@ -7,9 +7,9 @@ set -a; source pool.k8s-hosts/metadata; set +a
 #target the kube cluster
 ssh-keygen -N "" -f /root/.ssh/id_rsa
 sshpass -e ssh-copy-id -o StrictHostKeyChecking=no ${K8S_USER}@${K8S_HOST_IP}
-kubectl config set-cluster --server=$K8S_HOST_IP:$K8S_HOST_PORT $K8S_HOSTNAME
-kubectl config set-context $K8S_HOSTNAME --cluster=$K8S_HOSTNAME
-kubectl config use-context $K8S_HOSTNAME
+kubectl config set-cluster --server=${K8S_HOST_IP}:${K8S_HOST_PORT} ${K8S_HOSTNAME}
+kubectl config set-context ${K8S_HOSTNAME} --cluster=${K8S_HOSTNAME}
+kubectl config use-context ${K8S_HOSTNAME}
 
 unzip s3.scf-kube-yml/scf-kube-* -d scf-kube-yml
 
