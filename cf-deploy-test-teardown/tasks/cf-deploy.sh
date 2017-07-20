7c826efdc040b56b46a99b1d236c01e419ea13f5
Prabal Sharma <prabal.sharma@suse.com>
2017-07-20 14:37:14 -0700

    use helm to deploy cf

--- cf-deploy-test-teardown/tasks/cf-deploy.sh
+++ cf-deploy-test-teardown/tasks/cf-deploy.sh
@@ -16,13 +16,28 @@ kubectl config set-context ${K8S_HOSTNAME} --cluster=${K8S_HOSTNAME}
 kubectl config use-context ${K8S_HOSTNAME}
 
 unzip s3.scf-kube-yml/scf-kube-* -d scf-kube-yml
+unzip s3.scf-helm-charts/hcf-kube-charts-* -d scf-helm-charts
 
 #Deploy UAA
 kubectl create namespace uaa
-kubectl create -n uaa -f scf-kube-yml/uaa/bosh/
-kubectl create -n uaa -f scf-kube-yml/uaa/kube-test/exposed-ports.yml
+#kubectl create -n uaa -f scf-kube-yml/uaa/bosh/
+#kubectl create -n uaa -f scf-kube-yml/uaa/kube-test/exposed-ports.yml
+helm install scf-helm-charts/uaa/helm \
+     --namespace "uaa" \
+     --set "env.DOMAIN=${DOMAIN}" \
+     --set "env.UAA_ADMIN_CLIENT_SECRET=${UAA_ADMIN_CLIENT_SECRET}" \
+     --set "kube.external_ip=${K8S_HOST_IP}"
+
 
 #Deploy CF
 kubectl create namespace cf
-kubectl create -n cf -f scf-kube-yml/cf/bosh
-kubectl create -n cf -f scf-kube-yml/cf/bosh-task/post-deployment-setup.yml
+#kubectl create -n cf -f scf-kube-yml/cf/bosh
+#kubectl create -n cf -f scf-kube-yml/cf/bosh-task/post-deployment-setup.yml
+helm install scf-helm-charts/cf/helm \
+     --namespace "cf" \
+     --set "env.CLUSTER_ADMIN_PASSWORD=$CLUSTER_ADMIN_PASSWORD" \
+     --set "env.DOMAIN=${DOMAIN}" \
+     --set "env.UAA_ADMIN_CLIENT_SECRET=${UAA_ADMIN_CLIENT_SECRET}" \
+     --set "env.UAA_HOST=${UAA_HOST}" \
+     --set "env.UAA_PORT=${UAA_PORT}" \
+     --set "kube.external_ip=${K8S_HOST_IP}"
