c115878a150e217a4760731313bc68f30cd498b1
Alex Harford <alex.harford@suse.com>
2017-08-14 11:29:11 -0700

    More defensive variable checks
    * Fail if target isn't specified, add some usage info
    * Fail is CONCOURSE_SECRETS_FILE isn't defined

--- setup-opensuse-kubectl-docker.sh
+++ setup-opensuse-kubectl-docker.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-target="$1"
+set -o nounset
+set -o errexit
 
-# EV 'PIPELINE_PREFIX' = prefix to pipeline name for local
-#                        customization of test pipelines
+# EV 'PIPELINE_PREFIX' = prefix to pipeline name for local customization of test
+#           pipelines
+
+PIPELINE_NAME=cf-ci-orchestration
+
+if test -z "${1:-}"; then
+    printf "Usage:\n%s <target>\n" "${0}" >&2
+    exit 1
+fi
+
+target="${1}"
 
 if test -n "${CONCOURSE_SECRETS_FILE:-}"; then
     if test -r "${CONCOURSE_SECRETS_FILE:-}" ; then
@@ -12,13 +22,16 @@ if test -n "${CONCOURSE_SECRETS_FILE:-}"; then
         printf "ERROR: Secrets file %s is not readable\n" "${CONCOURSE_SECRETS_FILE}" >&2
         exit 2
     fi
+else
+    echo "ERROR: CONCOURSE_SECRETS_FILE location is not set" >&2
+    exit 3
 fi
 
-fly -t "$target" set-pipeline \
-    -p ${PIPELINE_PREFIX}cf-ci-orchestration \
-    -c cf-ci-orchestration/cf-ci-orchestration.yml \
+fly -t "${target}" set-pipeline \
+    -p "${PIPELINE_PREFIX}${PIPELINE_NAME}" \
+    -c "${PIPELINE_NAME}/${PIPELINE_NAME}.yml" \
     -v s3-bucket=cf-opensusefs2 \
     -l <(gpg -d --no-tty "${secrets_file}" 2> /dev/null)
 
-fly -t "$target" expose-pipeline  -p ${PIPELINE_PREFIX}cf-ci-orchestration
-fly -t "$target" unpause-pipeline -p ${PIPELINE_PREFIX}cf-ci-orchestration
+fly -t "${target}" expose-pipeline  -p "${PIPELINE_PREFIX}${PIPELINE_NAME}"
+fly -t "${target}" unpause-pipeline -p "${PIPELINE_PREFIX}${PIPELINE_NAME}"
