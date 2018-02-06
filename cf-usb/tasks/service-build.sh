35a1c756c8458ef3a84d64edbcaf98d2d9bbac80
Mark Yen <mark.yen@suse.com>
2018-02-06 13:17:22 -0800

    cf-usb: merge mysql/postgres task config
    They were the same file with a different `run` argument anyway; just use
    an environment variable set in the main pipeline instead.

--- cf-usb/tasks/service-build.sh
+++ cf-usb/tasks/service-build.sh
@@ -2,9 +2,8 @@
 set -o nounset -o errexit -o xtrace
 export GOPATH="${PWD}"
 export START_DIR="${PWD}"
-service=$1
 usbroot=src/github.com/SUSE/cf-usb-sidecar
-svcroot="${usbroot}/csm-extensions/services/dev-${service}"
+svcroot="${usbroot}/csm-extensions/services/dev-${SERVICE}"
 
 # Trigger generation of proper APP_VERSION_TAG
 export CONCOURSE_BUILD=1
@@ -29,4 +28,4 @@ if test -z "${APP_VERSION_TAG:-}" ; then
     APP_VERSION_TAG="$(cd "${usbroot}" && scripts/build_version.sh "APP_VERSION_TAG")"
 fi
 echo "${APP_VERSION_TAG}" > docker-out/tag
-tar -czf helm-out/cf-usb-sidecar-${service}-${APP_VERSION_TAG}.tgz -C "${svcroot}/output/helm/" .
+tar -czf helm-out/cf-usb-sidecar-${SERVICE}-${APP_VERSION_TAG}.tgz -C "${svcroot}/output/helm/" .
