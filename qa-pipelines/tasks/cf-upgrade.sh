b9f869c87b65b7af7d43e7d4911e08d2c9b47346
Hart Simha <hart.simha@suse.com>
2018-03-09 15:19:45 -0800

    Monitor go-env for downtime through upgrade

--- qa-pipelines/tasks/cf-upgrade.sh
+++ qa-pipelines/tasks/cf-upgrade.sh
@@ -98,7 +98,48 @@ wait_for_namespace() {
 
 PROVISIONER=$(kubectl get storageclasses persistent -o "jsonpath={.provisioner}")
 
-# Deploy UAA
+# monitor_url takes a URL argument and a path to a log file
+# This will time out after 3 hours. Until then, repeatedly curl the URL with a 1-second wait period, and log the response
+# If the application state changes, print this to stdout as well
+monitor_url() {
+  local app_url=$1
+  local log_file=$2
+  local count=0
+  local last_state=
+  local new_state=
+  echo "monitoring URL ${app_url}"
+  while true; do
+    new_state=$({ curl -sI "${1}" || echo "URL could not be reached"; } | head -n1 | tee -a "${log_file}")
+    if [[ "${new_state}" != "${last_state}" ]]; then
+      echo "state change for ${1}: ${new_state}"
+      last_state=$new_state
+    fi
+    ((++count))
+    if [[ ${count} -gt 10800 ]]; then
+      echo "Ending monitor of ${app_url} due to timeout"
+      break
+    fi
+    sleep 1
+  done
+}
+
+# push app in subshell to avoid changing directory
+(
+  cd ci/sample-apps/go-env
+  cf api --skip-ssl-validation "https://api.${DOMAIN}"
+  cf login -u admin -p changeme
+  cf create-org testorg
+  cf target -o testorg
+  cf create-space testspace
+  cf target -o testorg -s testspace
+  instance_count=$(helm get scf  | ruby -rjson -ryaml -e 'puts YAML.load(ARGF.read)["sizing"]["diego_cell"]["count"]')
+  cf push -i ${instance_count}
+)
+
+monitor_file=$(mktemp -d)/downtime.log
+monitor_url "http://go-env.${DOMAIN}" "${monitor_file}" &
+
+# Upgrade UAA
 helm upgrade uaa ${CAP_DIRECTORY}/helm/uaa${CAP_CHART}/ \
     --namespace "${UAA_NAMESPACE}" \
     --timeout 600 \
@@ -117,7 +158,7 @@ get_uaa_secret () {
 
 CA_CERT="$(get_uaa_secret internal-ca-cert | base64 -d -)"
 
-# Deploy CF
+# Upgrade CF
 if [[ ${HA} == true ]]; then
   HELM_PARAMS+=(--set=sizing.{diego_access,mysql}.count=1)
   HELM_PARAMS+=(--set=sizing.{api,cf_usb,diego_brain,doppler,loggregator,nats,router,routing_api}.count=2)
@@ -135,3 +176,20 @@ helm upgrade scf ${CAP_DIRECTORY}/helm/cf${CAP_CHART}/ \
 
 # Wait for CF namespace
 wait_for_namespace "${CF_NAMESPACE}"
+# While the background app monitoring job is running, *and* the app isn't yet ready, sleep
+while jobs %% &>/dev/null && ! tail -1 ${monitor_file} | grep -q "200 OK"; do
+  sleep 1
+done
+
+# If we get here because the app is ready, monitor_url will still be running in the background
+# Kill it, so we don't get any messages about the app becoming unreachable at the end
+if jobs %% &>/dev/null; then
+  echo "Terminating app monitoring background job"
+  kill %1
+fi
+
+echo "Results of app monitoring:"
+echo "SECONDS|STATUS"
+uniq -c "${monitor_file}"
+cf delete -f go-env
+cf delete-org -f testorg
