8185fdd388affd5a84e0e492abe49e09913dad9e
Christian Richter <crichter@suse.com>
2018-04-19 10:59:49 +0200

    Incorporate changes of cf-usb commit sources task
    Signed-off-by: Tim Hardeck <thardeck@suse.de>

--- cf-usb/tasks/service-commit-sources.sh
+++ cf-usb/tasks/service-commit-sources.sh
@@ -6,9 +6,9 @@
 set -o errexit
 
 if [[ "${COMMIT_SOURCES}" != "true" ]]; then
-  echo "+------------------------------------------------+"
-  echo "| On openSUSE builds no source will be published |"
-  echo "+------------------------------------------------+"
+  echo "+-----------------------------------------------+"
+  echo "| Publishing sources is disabled for this build |"
+  echo "+-----------------------------------------------+"
   exit 0;
 fi
 
@@ -21,16 +21,13 @@ sed -i "s|<username>|$OBS_USERNAME|g" /root/.oscrc
 sed -i "s|<password>|$OBS_PASSWORD|g" /root/.oscrc
 
 mkdir -p "${OSC_TARGET}"
-pushd "${OSC_TARGET}"
+cd "${OSC_TARGET}"
 osc checkout -M "${OSC_BASE_PATH}"
-pushd "${OSC_BASE_PATH}"
+cd "${OSC_BASE_PATH}"
 if [ ! -d "${PACKAGE}" ]; then
   osc mkpac "${PACKAGE}"
 fi
-pushd "${PACKAGE}"
+cd "${PACKAGE}"
 tar cfz "cf-usb-sidecar.tar.gz" -C "../../../src/" ./
 osc addremove
 osc commit -m "Add sources"
-popd
-popd
-popd
