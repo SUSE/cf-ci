ef973f1c78ffe787fc2807dcbc78aa478cc7e9a4
Prabal Sharma <prabal.sharma@suse.com>
2018-05-10 14:23:12 -0700

    fix sizing parameter names

--- qa-pipelines/tasks/cf-upgrade.sh
+++ qa-pipelines/tasks/cf-upgrade.sh
@@ -168,7 +168,7 @@ fi
 
 if [[ ${SCALED_HA} == true ]]; then
   HELM_PARAMS+=(--set=sizing.routing_api.count=1)
-  HELM_PARAMS+=(--set=sizing.{api,cc-uploader,cc-worker,cf_usb,diego_access,diego_brain,doppler,loggregator,mysql,nats,router,syslog-adapter,syslog-rlp,tcp-router,mysql-proxy}.count=2)
+  HELM_PARAMS+=(--set=sizing.{api,cc_uploader,cc_worker,cf_usb,diego_access,diego_brain,doppler,loggregator,mysql,nats,router,syslog_adapter,syslog_rlp,tcp_router,mysql_proxy}.count=2)
   HELM_PARAMS+=(--set=sizing.{diego_api,diego-locket,diego_cell}.count=3)
 fi
 
