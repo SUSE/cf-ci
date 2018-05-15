75716956d3160ee510de00f531797fb922646a67
Hart Simha <hart.simha@suse.com>
2018-05-15 07:42:45 -0700

    Use env vars in test setup if supplied

--- user-acceptance-tests/test-setup.sh
+++ user-acceptance-tests/test-setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #Export CF api endpoint and create first org and space
 
-export CF_TEST_TARGET_HOST=https://api.10.84.101.205.nip.io
-cf api --skip-ssl-validation ${CF_TEST_TARGET_HOST}
-cf login -u admin -p changeme
-cf create-org SUSE
-cf create-space -o SUSE QA
-cf target -o SUSE -s QA
+export CF_TEST_TARGET_HOST=${CF_TEST_TARGET_HOST:-https://api.10.84.101.205.nip.io}
+export CF_TEST_GLOBAL_ADMIN_USER=${CF_TEST_GLOBAL_ADMIN_USER:-admin}
+export CF_TEST_GLOBAL_ADMIN_PASS=${CF_TEST_GLOBAL_ADMIN_PASS:-changeme}
+export CF_TEST_GLOBAL_ADMIN_ORG=${CF_TEST_GLOBAL_ADMIN_ORG:-SUSE}
+export CF_TEST_GLOBAL_ADMIN_SPACE=${CF_TEST_GLOBAL_ADMIN_SPACE:-QA}
+cf api --skip-ssl-validation "${CF_TEST_TARGET_HOST}"
+cf login -u "${CF_TEST_GLOBAL_ADMIN_USER}" -p "${CF_TEST_GLOBAL_ADMIN_PASS}""
+cf create-org "${CF_TEST_GLOBAL_ADMIN_ORG}"
+cf create-space -o "${CF_TEST_GLOBAL_ADMIN_ORG}" "${CF_TEST_GLOBAL_ADMIN_SPACE}"
+cf target -o "${CF_TEST_GLOBAL_ADMIN_ORG}" -s "${CF_TEST_GLOBAL_ADMIN_SPACE}"
 
-#behave --logging-level=ERROR
-CF_TEST_TARGET_HOST=https://api.10.84.101.205.nip.io behave
+behave
