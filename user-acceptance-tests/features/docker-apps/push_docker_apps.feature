b36cade4aaac170165c127b3d9b4d66248bf754e
Prabal Sharma <prabal.sharma@suse.com>
2018-05-15 16:57:03 -0700

    fix typos in feature text, remove extra docker_apps_steps

--- user-acceptance-tests/features/docker-apps/push_docker_apps.feature
+++ user-acceptance-tests/features/docker-apps/push_docker_apps.feature
@@ -2,11 +2,11 @@
 
 Feature: Push Docker apps from the CLI
   As a Developer
-  I want to be able to to deploy a Docker images
+  I want to be able to deploy Docker images
 
   Scenario: Deploy image from Docker Hub
     Given the docker image 'viovanov/node-env-tiny' from Docker Hub exists
-    And admin have enabled diego_docker feature-flag
+    And admin has enabled diego_docker feature-flag
     And I am logged in as the developer user
     When I push a docker app 'viovanov/node-env-tiny' as 'uat-dockerapp'
     Then 'uat-dockerapp' should be deployed
--- user-acceptance-tests/features/steps/docker_apps_steps.py
+++ user-acceptance-tests/features/steps/docker_apps_steps.py
@@ -13,21 +13,10 @@ def docker_hub_check(context, docker_image):
     assert req.status_code is 200
 
 
-@given("Admin have enabled diego_docker feature-flag")
+@given("Admin has enabled diego_docker feature-flag")
 def enabled_diego_docker(context):
     context.CLI.execute_cmd(
         "enable-feature-flag diego_docker")
-
-@given("I have the docker image '{docker_image}' deployed as '{appname}'")
-def docker_image_deployed(context, docker_image, appname):
-    context.execute_steps(u'''
-        Given the docker image '{0}' from Docker Hub exists
-        And I am logged in as the admin user
-        And I have enabled diego_docker feature-flag
-        When I push a docker app '{0}' as '{1}'
-        Then '{1}' should be deployed
-    '''.format(docker_image, appname))
-
 
 @given("I push a docker app '{docker_image}' as '{appname}'")
 @when("I push a docker app '{docker_image}' as '{appname}'")
@@ -36,12 +25,3 @@ def docker_push_check(context, docker_image, appname):
         appname, docker_image)
     context.return_code, context.stdout, context.stderr = \
         context.CLI.execute_cmd(command)
-
-
-# "Try" means failure is allowed
-@when("I try to push a docker app '{docker_image}' as '{appname}'")
-def try_docker_push_check(context, docker_image, appname):
-    command = 'push {1} -o {0}'.format(
-        appname, docker_image)
-    context.return_code, context.stdout, context.stderr = \
-        context.CLI.execute_cmd(command, no_exception=True)
