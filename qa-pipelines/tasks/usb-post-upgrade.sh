950bf64ce0469bcbe78895895394d6c3eb4f3b2e
Hart Simha <hart.simha@suse.com>
2018-05-23 18:31:51 -0700

    Disable pre-upgrade brains, workaround known issue
    - The acceptance-tests-brain suite is currently known to not work with
      2.8.0, so for now (specifically the usb test fails), so we should
      disable it until our next release.
    - Upgrading to 2.10.1 with a secret rotation will break the cloud
      controller communication with USB, until its broker password is
      updated with a workaround

--- qa-pipelines/tasks/usb-post-upgrade.sh
+++ qa-pipelines/tasks/usb-post-upgrade.sh
@@ -2,6 +2,14 @@
 set -o errexit -o nounset
 
 DOMAIN=$(kubectl get pods -o json --namespace scf api-0 | jq -r '.spec.containers[0].env[] | select(.name == "DOMAIN").value')
+
+# Temporary workaround for usb breakage after secret rotation
+CF_NAMESPACE=scf
+SECRET=$(kubectl get --namespace $CF_NAMESPACE deploy -o json | jq -r '[.items[].spec.template.spec.containers[].env[] | select(.name == "INTERNAL_CA_CERT").valueFrom.secretKeyRef.name] | unique[]')
+USB_PASSWORD=$(kubectl get -n scf secret $SECRET -o jsonpath='{@.data.cf-usb-password}' | base64 -d)
+USB_ENDPOINT=$(cf curl /v2/service_brokers | jq -r '.resources[] | select(.entity.name=="usb").entity.broker_url')
+cf update-service-broker usb broker-admin "$USB_PASSWORD" "$USB_ENDPOINT"
+
 curl https://scf-rails-example.$DOMAIN
 cd rails-example
 cf target -o usb-test-org -s usb-test-space
