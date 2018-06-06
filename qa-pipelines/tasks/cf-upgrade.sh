35de3ac35d71ab5447584d77d8c2538c8c038a99
Hart Simha <hart.simha@suse.com>
2018-06-06 09:44:12 -0700

    Get DOMAIN from api pod for upgrade

--- qa-pipelines/tasks/cf-upgrade.sh
+++ qa-pipelines/tasks/cf-upgrade.sh
@@ -6,20 +6,19 @@ mkdir -p /root/.kube/
 cp  pool.kube-hosts/metadata /root/.kube/config
 
 set -o allexport
-# The IP address assigned to the first kubelet node.
-external_ip=$(kubectl get nodes -o json | jq -r '.items[] | select(.spec.unschedulable == true | not) | .metadata.annotations["alpha.kubernetes.io/provided-node-ip"]' | head -n1)
-# Domain for SCF. DNS for *.DOMAIN must point to the kube node's
-# external ip. This must match the value passed to the
-# cert-generator.sh script.
-DOMAIN=${external_ip}.${MAGIC_DNS_SERVICE}
+CF_NAMESPACE=scf
+UAA_NAMESPACE=uaa
+# Domain for SCF, taken from the api pod
+DOMAIN=$(kubectl get pods -o json --namespace ${CF_NAMESPACE} api-0 | jq -r  '.spec.containers[0].env[] |  select(.name == "DOMAIN").value')
+api_prefix=${DOMAIN%.${MAGIC_DNS_SERVICE}}
+external_ip=${api_prefix#api.}
+unset api_prefix
 # Password for SCF to authenticate with UAA
 UAA_ADMIN_CLIENT_SECRET="$(head -c32 /dev/urandom | base64)"
 # UAA host/port that SCF will talk to.
 UAA_HOST=uaa.${external_ip}.${MAGIC_DNS_SERVICE}
 UAA_PORT=2793
 
-CF_NAMESPACE=scf
-UAA_NAMESPACE=uaa
 CAP_DIRECTORY=s3.scf-config
 set +o allexport
 
