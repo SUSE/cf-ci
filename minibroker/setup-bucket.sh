044275549d0772752b444eb8c0e191e438aeef46
Alex Harford <alex.harford@suse.com>
2018-07-04 08:55:05 -0700

    Use a more obvious default timestamp

--- minibroker/setup-bucket.sh
+++ minibroker/setup-bucket.sh
@@ -10,7 +10,7 @@ aws s3api put-bucket-versioning --bucket minibroker-helm-charts --versioning-con
 cat <<EOF > index.yaml
 apiVersion: v1
 entries: ~
-generated: 2018-06-28T21:56:27.538677094Z
+generated: 1970-01-01T00:00:00.000000000Z
 EOF
 
 s3cmd put --acl-public index.yaml s3://minibroker-helm-charts/kubernetes-charts/index.yaml
