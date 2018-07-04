27f9ac3d199d483d354076a13ac181f6648cc642
Alex Harford <alex.harford@suse.com>
2018-07-04 13:49:33 -0700

    Fix location for PR chart files

--- minibroker/scripts/generate-chart.sh
+++ minibroker/scripts/generate-chart.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 set -e
 set -x
-helm package --save=false --destination chart/ "src/stable/${CHART_NAME}"
+VERSION=$(awk '/^version:/{print $2}' "src/stable/${CHART_NAME}/Chart.yaml")-pr$(cat src/.git/id)
+helm package --version "${VERSION}" --save=false --destination chart/ "src/stable/${CHART_NAME}"
