784204c59701596dd38647b1e718b4621633af61
Alex Harford <alex.harford@suse.com>
2018-07-23 09:37:16 -0700

    Clean up bucket setup script
    * Use `aws` tool exclusively as it supports the `put-bucket-versioning`
      command
    * Allow the bucket name and dir to be set as arguments to the script.
    * Grab the defaults from the `config-production.yaml` file.

--- minibroker/setup-bucket.sh
+++ minibroker/setup-bucket.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env sh
 
+# Usage:
+# setup-bucket.sh <bucket name> <dir name>
+#
+# Stores the index.yaml file in s3://<bucket name>/<dir name>/index.yaml
+# and configures versioning for it.
+#
+# If the bucket name and dir name aren't specified, the defaults will be taken
+# from the pipeline's config-production.yaml file.
+
 set -e
-set -x
 
-s3cmd mb s3://minibroker-helm-charts
+HELM_CHARTS_BUCKET=${1:-$(awk '/^s3-bucket/ {print $2}' < config-production.yaml)}
+HELM_CHARTS_DIR=${2:-$(awk '/^helm-charts-s3-prefix/ {print $2}' < config-production.yaml)}
 
-aws s3api put-bucket-versioning --bucket minibroker-helm-charts --versioning-configuration Status=Enabled
+aws s3api create-bucket --bucket "${HELM_CHARTS_BUCKET}"
 
-cat <<EOF > index.yaml
+aws s3api put-bucket-versioning --bucket "${HELM_CHARTS_BUCKET}" --versioning-configuration Status=Enabled
+
+INDEX_YAML=$(mktemp)
+
+cat <<EOF > "${INDEX_YAML}"
 apiVersion: v1
 entries: ~
 generated: 1970-01-01T00:00:00.000000000Z
 EOF
 
-s3cmd put --acl-public index.yaml s3://minibroker-helm-charts/kubernetes-charts/index.yaml
+aws s3api put-object --acl public-read --bucket "${HELM_CHARTS_BUCKET}" --body "${INDEX_YAML}" --key "${HELM_CHARTS_DIR}/index.yaml"
+
+rm "${INDEX_YAML}"
