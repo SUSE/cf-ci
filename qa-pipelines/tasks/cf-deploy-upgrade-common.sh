429a977a6e1805817a3e92a6f5e7d7a8d431b45e
Prabal Sharma <prabal.sharma@suse.com>
2018-10-01 09:35:32 -0700

    update ips

--- qa-pipelines/tasks/cf-deploy-upgrade-common.sh
+++ qa-pipelines/tasks/cf-deploy-upgrade-common.sh
@@ -172,8 +172,8 @@ set -o allexport
 #public_ip=$(kubectl get configmap -n kube-system cap-values -o json | jq -r '.data["public-ip"]')
 #garden_rootfs_driver=$(kubectl get configmap -n kube-system cap-values -o json | jq -r '.data["garden-rootfs-driver"] // "btrfs"')
 
-external_ip=192.168.214.176
-public_ip=54.202.191.90
+external_ip=192.168.203.5
+public_ip=52.37.3.144
 
 # Domain for SCF. DNS for *.DOMAIN must point to the same kube node
 # referenced by external_ip.
