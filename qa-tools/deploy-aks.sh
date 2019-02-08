682216285a7941c558e503c9df22b8be47601fde
Hart Simha <hart.simha@suse.com>
2019-02-08 15:27:36 -0800

    Let CAP charts manage LB for AKS deploys

--- qa-tools/deploy-aks.sh
+++ qa-tools/deploy-aks.sh
@@ -93,79 +93,16 @@ for i in $vmnodes; do
    az vm restart -g $AZ_MC_RG_NAME -n $i
 done
 
-az network public-ip create \
-  --resource-group $AZ_MC_RG_NAME \
-  --name $AZ_AKS_NAME-public-ip \
-  --allocation-method Static
-
-az network lb create \
-  --resource-group $AZ_MC_RG_NAME \
-  --name $AZ_AKS_NAME-lb \
-  --public-ip-address $AZ_AKS_NAME-public-ip \
-  --frontend-ip-name $AZ_AKS_NAME-lb-front \
-  --backend-pool-name $AZ_AKS_NAME-lb-back
-
-AZ_NIC_NAMES=$(az network nic list --resource-group $AZ_MC_RG_NAME | jq -r '.[].name')
-for i in $AZ_NIC_NAMES; do
-  az network nic ip-config address-pool add \
-    --resource-group $AZ_MC_RG_NAME \
-    --nic-name $i \
-    --ip-config-name ipconfig1 \
-    --lb-name $AZ_AKS_NAME-lb \
-    --address-pool $AZ_AKS_NAME-lb-back
-done
-
-export CAP_PORTS="80 443 4443 2222 2793 8443 $(echo 2000{0..9})"
-
-for i in $CAP_PORTS; do
-  az network lb probe create \
-    --resource-group $AZ_MC_RG_NAME \
-    --lb-name $AZ_AKS_NAME-lb \
-    --name probe-$i \
-    --protocol tcp \
-    --port $i 
-    
-  az network lb rule create \
-    --resource-group $AZ_MC_RG_NAME \
-    --lb-name $AZ_AKS_NAME-lb \
-    --name rule-$i \
-    --protocol Tcp \
-    --frontend-ip-name $AZ_AKS_NAME-lb-front \
-    --backend-pool-name $AZ_AKS_NAME-lb-back \
-    --frontend-port $i \
-    --backend-port $i \
-    --probe probe-$i 
-done
-
-az network lb rule list -g $AZ_MC_RG_NAME --lb-name $AZ_AKS_NAME-lb | grep -i port
-
-nsg=$(az network nsg list --resource-group=$AZ_MC_RG_NAME | jq -r '.[].name')
-pri=200
-
-for i in $CAP_PORTS; do
-  az network nsg rule create \
-    --resource-group $AZ_MC_RG_NAME \
-    --priority $pri \
-    --nsg-name $nsg \
-    --name $AZ_AKS_NAME-$i \
-    --direction Inbound \
-    --destination-port-ranges $i \
-    --access Allow
-  pri=$(expr $pri + 1)
-done
-
 internal_ips=($(az network nic list --resource-group $AZ_MC_RG_NAME | jq -r '.[].ipConfigurations[].privateIpAddress'))
-public_ip=$(az network public-ip show --resource-group $AZ_MC_RG_NAME --name $AZ_AKS_NAME-public-ip --query ipAddress --output tsv)
 echo -e "\n Resource Group:\t$AZ_RG_NAME\n \
-Public IP:\t\t${public_ip}\n \
 Private IPs:\t\t\"$(IFS=,; echo "${internal_ips[*]}")\"\n"
 
 # TODO: check if -i and -it are really needed in the following 3 commands
 docker exec -it aks-deploy kubectl create configmap -n kube-system cap-values \
   --from-literal=internal-ip=${internal_ips[0]} \
-  --from-literal=public-ip=$public_ip \
   --from-literal=garden-rootfs-driver=overlay-xfs \
   --from-literal=platform=azure \
+  --from-literal=resource-group=$AZ_RG_NAME \
   --from-literal="node-ssh-access=$(cat $AZ_SSH_KEY)"
 rm -rf "/tmp/tmp.${AZ_SSH_KEY_PATH##/tmp/tmp.}"
 cat persistent-sc.yaml cluster-admin.yaml | docker exec -i aks-deploy kubectl create -f -
