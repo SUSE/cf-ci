9ae18d0b17bb9b659b4efb3fc164f5a6872762c4
Satadru Roy <sroy@suse.com>
2019-03-14 23:02:45 -0700

    GKE automation script

---
+++ qa-tools/deploy-gke.sh
@@ -0,0 +1,128 @@
+usage() {
+  cat << 'EOF'
+  Usage:
+  deploy-gke.sh [SA_NAME] [KEY_FILE] [PROJ_ID] (optional)
+  Requirements:
+  * Your GKE user/service account user needs the following roles:
+    container.admin compute.admin iam.serviceAccountUser
+    You can grant roles to a service account via the following, for a service account sa for project 
+    proj-id:
+    `gcloud projects add-iam-policy-binding <proj-id> --member \ 
+     serviceAccount:sa@proj-id.iam.gserviceaccount.com --role \ 
+     roles/container.admin --role roles/compute.admin --role roles/iam.serviceAccountUser`
+    etc.
+EOF
+}
+
+if [ $# -lt 2 ]
+then
+	usage
+	exit 0
+fi
+
+
+cleanup() {
+  for container in "${CONTAINERS_TMP[@]}"; do
+    if docker container ls -a --format '{{.Names}}' | grep -Eq "^$container\$"; then
+      docker container rm --force $container
+    fi
+  done
+  for path in "${PATHS_TMP[@]}"; do
+    # Only cleanup tmp paths in /tmp/
+    if [[ -d "${path}" ]] && [[ ${path} =~ ^/tmp/ ]]; then
+      rm -rf "${path}"
+    fi
+  done
+  for file in "${FILES_TMP[@]}"; do
+    if [[ -f "${file}" ]]; then
+      rm -f "${file}"
+    fi
+  done
+}
+
+CONTAINERS_TMP=(gke-deploy)
+PATHS_TMP=()
+FILES_TMP=()
+trap cleanup EXIT
+
+set -o errexit
+postfix=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
+export CLUSTER_NAME=${CLUSTER_NAME-cap-gke-${postfix}}
+export CLUSTER_ZONE=${CLUSTER_ZONE-us-west1-a}
+
+#  We are setting up a zonal cluster, not a regional one  and therefore we're setting the cluster zone
+# and node location to be the same to avoid spinning up 9 nodes (default behaviour of GKE container cluster
+# create
+
+export NODE_LOCATIONS=${CLUSTER_ZONE}
+export NODE_COUNT=3
+export SA_USER=$1
+export KEY_FILE=$2
+export PROJECT=${3-suse-css-platform}
+# Log in with the GKE service account
+gcloud auth activate-service-account $1 --key-file $2 --project=$3
+
+# Create GKE cluster 
+
+gcloud container clusters create ${CLUSTER_NAME} --image-type=UBUNTU --machine-type=n1-standard-4 --zone \
+${CLUSTER_ZONE} --num-nodes=$NODE_COUNT --node-locations ${NODE_LOCATIONS}
+
+if [ "$(uname)" == "Darwin" ]; then
+        export KUBECONFIG=/tmp/config
+else
+	export KUBECONFIG=$(mktemp -d)/config
+fi
+
+# All future kubectl commands will be run in this container. This ensures the
+# correct version of kubectl is used, and that it matches the version used by CI
+docker container run \
+  --name gke-deploy \
+  --detach \
+  --volume $2:/root/.kube/sa-key \
+  cf-ci-orchestration:local tail -f /dev/null
+
+docker container exec gke-deploy gcloud auth activate-service-account $1 --key-file /root/.kube/sa-key --project=$3
+docker container exec gke-deploy gcloud container clusters get-credentials  ${CLUSTER_NAME} --zone ${CLUSTER_ZONE:?required}
+
+checkready() {
+	while [[ $node_readiness != "$NODE_COUNT True" ]]; do
+		sleep 10
+		node_readiness=$(
+			docker container exec gke-deploy kubectl get nodes -o json \
+      		| jq -r '.items[] | .status.conditions[] | select(.type == "Ready").status' \
+      		| uniq -c | grep -o '\S.*'
+  		)
+	done
+}
+
+checkready
+
+if [ "$(uname)" == "Darwin" ]; then
+	args=I
+else
+	args=i
+fi
+
+#Grab node instance names
+instance_names=$(gcloud compute instances list --filter=name~${CLUSTER_NAME:?required} --format json | jq --raw-output '.[].name')
+
+# Set correct zone
+gcloud config set compute/zone ${CLUSTER_ZONE:?required}
+
+# Update kernel command line
+echo "$instance_names" | xargs -${args}{} gcloud compute ssh {} -- "sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=\"console=ttyS0 net.ifnames=0\"/GRUB_CMDLINE_LINUX_DEFAULT=\"console=ttyS0 net.ifnames=0 swapaccount=1\"/g' /etc/default/grub.d/50-cloudimg-settings.cfg"
+
+# Update grub
+echo "$instance_names" | xargs -${args}{} gcloud compute ssh {} -- "sudo update-grub"
+
+# Restart VMs
+echo "$instance_names" | xargs gcloud compute instances reset
+
+checkready
+
+# TODO: check if -i and -it are really needed in the following 3 commands
+docker exec -it gke-deploy kubectl create configmap -n kube-system cap-values \
+  --from-literal=garden-rootfs-driver=overlay-xfs \
+  --from-literal=platform=gke 
+cat gke-helm-sa.yaml | docker exec -i gke-deploy kubectl create -f -
+docker exec -it gke-deploy helm init
