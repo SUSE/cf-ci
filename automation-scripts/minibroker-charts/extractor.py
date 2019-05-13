346405a7385410b00010f736f46f674cd5d26a63
bikramnehra <bikramnehra@gmail.com>
2019-05-13 16:04:55 -0700

    add spcl char while constructing key & check args

--- automation-scripts/minibroker-charts/extractor.py
+++ automation-scripts/minibroker-charts/extractor.py
@@ -14,6 +14,12 @@ from ruamel.yaml import YAML
 
 HELM_REPO_URL="https://kubernetes-charts.storage.googleapis.com/index.yaml"
 
+def create_key(arg_list):
+    """
+    Method to create key from passed args
+    """
+    return "%".join(arg_list)
+
 def process_needles():
     """
     Method to load and process known working versions in versions.yaml.
@@ -37,7 +43,7 @@ def process_haystack(databases):
     for db in databases:
         for item in data["entries"][db]:
             if "appVersion" in item and "version" in item:
-                key = db+item["appVersion"]+item["version"]
+                key = create_key([db, item["appVersion"], item["version"]])
                 haystack[key] = item
 
     return haystack
@@ -51,22 +57,26 @@ def dump_yaml(data):
         yaml.dump(data, outfile)
 
 def main():
-    needles = process_needles()
-    databases = needles.keys()
-    haystack = process_haystack(databases)
-    output = {}
-    for db in databases:
-        for item in needles[db]:
-            needle = db+item["appVersion"]+item["version"]
-            # Check if provided version exists in helm stable repo.
-            if needle in haystack:
-                if db not in output:
-                    output[db] = [haystack[needle]]
-                else:
-                    output[db].append(haystack[needle])
+    if len(argv) < 3:
+        print("Please provide correct arguments...")
+        print("Usage: python extractor.py <path/to/versions.yaml> <path/to/output/index.yaml>")
+    else:    
+        needles = process_needles()
+        databases = needles.keys()
+        haystack = process_haystack(databases)
+        output = {}
+        for db in databases:
+            for item in needles[db]:
+                needle = create_key([db,item["appVersion"],item["version"]])
+                # Check if provided version exists in helm stable repo.
+                if needle in haystack:
+                    if db not in output:
+                        output[db] = [haystack[needle]]
+                    else:
+                        output[db].append(haystack[needle])
 
-    data = {"apiVersion": "v1", "entries": output}
-    dump_yaml(data)
+        data = {"apiVersion": "v1", "entries": output}
+        dump_yaml(data)
 
 if __name__ == "__main__":
     main()
