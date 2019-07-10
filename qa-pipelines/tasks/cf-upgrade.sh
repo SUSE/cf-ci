2a5d909399e0f441af9059c51a76aeed58b9ab5b
Prabal Sharma <prabal.sharma@suse.com>
2019-07-10 11:38:43 -0700

    sa-ha-upgrade

--- qa-pipelines/tasks/cf-upgrade.sh
+++ qa-pipelines/tasks/cf-upgrade.sh
@@ -54,7 +54,6 @@ echo "${HELM_PARAMS[@]}" | sed 's/kube\.registry\.password=[^[:space:]]*/kube.re
 
 helm upgrade uaa ${CAP_DIRECTORY}/helm/uaa/ \
     --namespace "${UAA_NAMESPACE}" \
-    --recreate-pods \
     --timeout 3600 \
     --wait \
     "${HELM_PARAMS[@]}"
@@ -73,7 +72,6 @@ echo "${HELM_PARAMS[@]}" | sed 's/kube\.registry\.password=[^[:space:]]*/kube.re
 
 helm upgrade scf ${CAP_DIRECTORY}/helm/cf/ \
     --namespace "${CF_NAMESPACE}" \
-    --recreate-pods \
     --timeout 3600 \
     --set "secrets.CLUSTER_ADMIN_PASSWORD=${CLUSTER_ADMIN_PASSWORD:-changeme}" \
     --set "env.UAA_HOST=${UAA_HOST}" \
