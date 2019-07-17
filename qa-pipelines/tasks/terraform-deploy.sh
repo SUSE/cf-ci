c322484f09441abdca1633bdc046dd448afdf88a
Prabal Sharma <prabal.sharma@suse.com>
2019-07-17 11:28:55 -0700

    move TF vars to env

--- qa-pipelines/tasks/terraform-deploy.sh
+++ qa-pipelines/tasks/terraform-deploy.sh
@@ -6,12 +6,6 @@ AZ_RG_NAME=${PIPELINE_NAME}
 az login --service-principal -u ${ARM_CLIENT_ID} --password ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID} &> /dev/null
 az group create --name ${AZ_RG_NAME} --location ${AZ_REGION} &> /dev/null
 
-export TF_VAR_client_id=${ARM_CLIENT_ID}
-export TF_VAR_client_secret=${ARM_CLIENT_SECRET}
-export TF_VAR_az_resource_group=${AZ_RG_NAME}
-export TF_VAR_cluster_labels=${AZ_RG_NAME}
-export TF_VAR_location="{"owner" = "${AZ_REGION}"}"
-
 cd ci/cap-terraform/aks/
 terraform init &> /dev/null
 terraform plan &> /dev/null
