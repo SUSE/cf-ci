1f0c30c828e5b4d0d45b13c8b9fe241da1721794
Prabal Sharma <prabal.sharma@suse.com>
2019-07-17 13:49:10 -0700

    use resource to add generated kubeconfig to pool

--- qa-pipelines/tasks/delete-aks-resource-group.sh
+++ qa-pipelines/tasks/delete-aks-resource-group.sh
@@ -2,7 +2,6 @@
 set -o errexit
 set -o nounset
 
-AZ_RG_NAME=${PIPELINE_NAME}
-
-az login --service-principal -u ${ARM_CLIENT_ID} --password ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID}
-az group delete -n ${AZ_RG_NAME}
+az login --service-principal -u ${ARM_CLIENT_ID} --password ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID} > /dev/null
+az group delete -n ${PIPELINE_NAME}
+echo "az group ${PIPELINE_NAME} deleted"
--- qa-pipelines/tasks/terraform-deploy.sh
+++ qa-pipelines/tasks/terraform-deploy.sh
@@ -2,18 +2,23 @@
 set -o errexit
 set -o nounset
 
-AZ_RG_NAME=${PIPELINE_NAME}
-az login --service-principal -u ${ARM_CLIENT_ID} --password ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID} &> /dev/null
-az group create --name ${AZ_RG_NAME} --location ${AZ_REGION} &> /dev/null
+az login --service-principal -u ${ARM_CLIENT_ID} --password ${ARM_CLIENT_SECRET} --tenant ${ARM_TENANT_ID} > /dev/null
+az group create --name ${PIPELINE_NAME} --location ${TF_VAR_location} > /dev/null
 
+helm init --client-only
+
+(
 cd ci/cap-terraform/aks/
-terraform init &> /dev/null
-terraform plan &> /dev/null
-terraform apply -auto-approve &> /dev/null
-git clone ${KUBE_POOL_REPO}
-cd ${KUBE_POOL_BRANCH}
-git checkout -t origin/${KUBE_POOL_BRANCH}
-cp ../aksk8scfg unclaimed/${PIPELINE_NAME}
-git add unclaimed/${PIPELINE_NAME}
+terraform init
+echo "Terraform plan in progress ..."
+terraform plan > /dev/null
+echo "Terraform apply in progress ..."
+terraform apply -auto-approve > /dev/null
+echo "Terraform apply succeeded"
+)
+
+git clone cf-ci-pools cf-ci-pools-terraform
+cd cf-ci-pools-terraform
+cp ../ci/cap-terraform/aks/aksk8scfg ${KUBE_POOL_POOL}/unclaimed/${PIPELINE_NAME}
+git add ${KUBE_POOL_POOL}/unclaimed/${PIPELINE_NAME}
 git commit -m "add ${PIPELINE_NAME}"
-git push
