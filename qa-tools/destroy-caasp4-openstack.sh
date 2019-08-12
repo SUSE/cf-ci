2e0e85297896f30e7560ea6570b343d0e986e612
Víctor Cuadrado Juan <vcuadradojuan@suse.de>
2019-08-12 15:35:05 +0200

    Add destroy-caasp4-openstack.sh

---
+++ qa-tools/destroy-caasp4-openstack.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Destroys an existing Caasp4 cluster on openstack
+#
+# Expected env var           Eg:
+#   VERSION                  devel, staging, product, update. default: devel
+#
+# Requirements:
+# - Built skuba docker image
+# - Sourced openrc.sh
+# - Key on the ssh keyring
+# - Run inside of a workspace of a deployed caasp4 in openstack
+
+if [[ ! -v VERSION ]]; then
+    export VERSION="devel"
+fi
+
+export SKUBA_TAG="$VERSION"
+SKUBA_DEPLOY_PATH=$(dirname "$(readlink -f "$0")")/skuba-deploy.sh
+skuba-deploy() {
+    bash "$SKUBA_DEPLOY_PATH" "$@"
+}
+
+export KUBECONFIG=""
+
+# check ssh key
+agent="$(pgrep ssh-agent -u "$USER")"
+if [[ "$agent" == "" ]]; then
+    eval "$(ssh-agent -s)"
+fi
+if ssh-add -L | grep -q 'ssh' ; then
+    curl https://raw.githubusercontent.com/SUSE/skuba/master/ci/infra/id_shared -o "$WORKSPACE"/id_rsa
+    ssh-add "$WORKSPACE"/id_rsa
+fi
+
+if [[ ! -v OS_PASSWORD ]]; then
+    echo ">>> Missing openstack credentials" && exit 1
+fi
+
+
+echo ">>> Destroying deployed openstack stack"
+skuba-deploy --run-in-docker terraform destroy -auto-approve
+echo ">>> Destroyed openstack stack"
