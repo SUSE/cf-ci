e2388401645ec99ea29dd2d6c21858e42e97e229
Bikramdeep Singh <bikramdeep.singh@suse.com>
2019-12-03 16:24:48 -0800

    changing logic of setting release version

--- kubecf-build-pipelines/build-releases/tasks/build_release.sh
+++ kubecf-build-pipelines/build-releases/tasks/build_release.sh
@@ -19,22 +19,7 @@ blobstore:
     secret_access_key: "$SECRET_ACCESS_KEY"
 EOF
 
-# Extract Major, Minor and Patch-Level version based on a regex (only for releases
-# who are versioned based on blobs.yml files).
-# For the rest, the next patchlevel is returned.
-if [ -n "${VERSION_REGEXP}" ]; then
-  VERSION=$(head -n2 config/blobs.yml | tail -n1 | perl -pe "s/$VERSION_REGEXP/\1/p")
-
-  echo "Will now generate version ${VERSION} (\$VERSION_REGEXP was used)"
-else
-  OLD_VERSION=$(git describe --tags --abbrev=0)
-  # Increase patch level by one (TODO: Find simpler way)
-  VERSION="$(echo $OLD_VERSION | sed -e 's/\(.*\)\.[0-9]\+/\1/').$(($(echo $OLD_VERSION | rev | cut -d. -f1 | rev)+1))"
-
-  echo "Old version is ${OLD_VERSION}"
-  echo "Will now generate version ${VERSION}"
-fi
-
+VERSION=$(git describe --tags --abbrev=0)
 RELEASE_TARBALL_BASE_NAME=${RELEASE_NAME}-release-${VERSION}.tgz
 RELEASE_TARBALL=$ROOT_DIR/release_tarball_dir/${RELEASE_TARBALL_BASE_NAME}
 
