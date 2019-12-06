7538cde4763d9ba3463bce16e39deb89fc0b0e80
Hart Simha <hart.simha@suse.com>
2019-12-06 05:37:07 -0800

    Repeat swapaccounting set steps until swapaccounting is set
    For unknown reasons, these don't always get set correctly on workers

--- cap-terraform/aks/aks-post-processing.sh
+++ cap-terraform/aks/aks-post-processing.sh
@@ -2,12 +2,20 @@
 
 export MCRGNAME=$(az aks show --resource-group $RGNAME --name $AKSNAME --query nodeResourceGroup -o json | jq -r '.')
 
-export VMNODES=$(az vm list --resource-group $MCRGNAME -o json | jq -r '.[] | select (.tags.poolName | contains("'$NODEPOOLNAME'")) | .name')
+set -- $(az vm list --resource-group $MCRGNAME -o json | jq -r '.[] | select (.tags.poolName | contains("'$NODEPOOLNAME'")) | .name')
 
-for i in $VMNODES
- do
-   az vm run-command invoke -g $MCRGNAME -n $i --command-id RunShellScript --scripts \
-   "sudo sed -i -r 's|^(GRUB_CMDLINE_LINUX_DEFAULT=)\"(.*.)\"|\1\"\2 swapaccount=1\"|' \
-   /etc/default/grub.d/50-cloudimg-settings.cfg && sudo update-grub"
-   az vm restart -g $MCRGNAME -n $i
+while [[ -n "${@}" ]]; do
+    for node in ${@}; do
+        az vm run-command invoke -g $MCRGNAME -n $node --command-id RunShellScript --scripts \
+            "sudo sed -i -r 's|^(GRUB_CMDLINE_LINUX_DEFAULT=)\"(.*.)\"|\1\"\2 swapaccount=1\"|' \
+            /etc/default/grub.d/50-cloudimg-settings.cfg && sudo update-grub"
+        az vm restart -g $MCRGNAME -n $node
+    done
+    nodes_without_swap=()
+    for node in ${@}; do
+        if ! az vm run-command invoke -g $MCRGNAME -n $node --command-id RunShellScript --scripts 'test -e "/sys/fs/cgroup/memory/memory.memsw.usage_in_bytes" && test -e "/sys/fs/cgroup/memory/memory.memsw.limit_in_bytes" && echo SwapAccountingOK' | grep -q SwapAccountingOK; then
+            nodes_without_swap+=($node)
+        fi
+    done
+    set -- "${nodes_without_swap[@]}"
 done
--- cap-terraform/gke/gke-post-processing.sh
+++ cap-terraform/gke/gke-post-processing.sh
@@ -9,33 +9,36 @@ gcloud config set project ${PROJECT}
 gcloud container clusters get-credentials  ${CLUSTER_NAME} --zone ${CLUSTER_ZONE:?required}
 
 checkready() {
-	while [[ $node_readiness != "$NODE_COUNT True" ]]; do
-		sleep 10
-		node_readiness=$(
-			kubectl get nodes -o json \
-      		| jq -r '.items[] | .status.conditions[] | select(.type == "Ready").status' \
-      		| uniq -c | grep -o '\S.*'
-  		)
-	done
+    while [[ $node_readiness != "$NODE_COUNT True" ]]; do
+        sleep 10
+        node_readiness=$(
+            kubectl get nodes -o json \
+                | jq -r '.items[] | .status.conditions[] | select(.type == "Ready").status' \
+                | uniq -c | grep -o '\S.*'
+        )
+    done
 }
 
 checkready
-
-if [ "$(uname)" == "Darwin" ]; then
-	args=I
-else
-	args=i
-fi
 echo "Setting swap accounting"
-
-#Grab node instance names
-instance_names=$(gcloud compute instances list --filter=name~${CLUSTER_NAME:?required} --format json | jq --raw-output '.[].name')
 
 # Set correct zone
 gcloud config set compute/zone ${CLUSTER_ZONE:?required}
 
-# Update kernel command line, update GRUB and reboot
-echo "$instance_names" | xargs -${args}{} gcloud compute ssh {} -- "sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=\"console=ttyS0 net.ifnames=0\"/GRUB_CMDLINE_LINUX_DEFAULT=\"console=ttyS0 net.ifnames=0 cgroup_enable=memory swapaccount=1\"/g' /etc/default/grub.d/50-cloudimg-settings.cfg && sudo update-grub && sudo systemctl reboot -i"
+#Grab node instance names
+set -- $(gcloud compute instances list --filter=name~${CLUSTER_NAME:?required} --format json | jq --raw-output '.[].name')
 
-echo "restarted the VMs"
-checkready
+while [[ -n "${@}" ]]; do
+    for node in ${@}; do
+        # Update kernel command line, update GRUB and reboot
+        gcloud compute ssh $node -- "sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=\"console=ttyS0 net.ifnames=0\"/GRUB_CMDLINE_LINUX_DEFAULT=\"console=ttyS0 net.ifnames=0 cgroup_enable=memory swapaccount=1\"/g' /etc/default/grub.d/50-cloudimg-settings.cfg && sudo update-grub && sudo systemctl reboot -i"
+    done
+    checkready
+    nodes_without_swap=()
+    for node in ${@}; do
+        if ! gcloud compute ssh $node -- 'sudo test -e "/sys/fs/cgroup/memory/memory.memsw.usage_in_bytes" && sudo test -e "/sys/fs/cgroup/memory/memory.memsw.limit_in_bytes" && echo SwapAccountingOK' | grep -q SwapAccountingOK; then
+            nodes_without_swap+=($node)
+        fi
+    done
+    set -- "${nodes_without_swap[@]}"
+done
